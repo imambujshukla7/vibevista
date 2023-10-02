493ed2e5bcfbe0243eb28ef0dbf04ccb284f18da
Ambuj Shukla <146608389+imambujshukla7@users.noreply.github.com>
2023-10-03 02:01:57 +0530

    Create helpers.js

---
+++ server/utils/helpers.js
@@ -0,0 +1,20 @@
+const generateRandomString = (length) => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(date).toLocaleDateString(undefined, options);
+};
+
+
+module.exports = {
+  generateRandomString,
+  formatDate,
+  
+};
