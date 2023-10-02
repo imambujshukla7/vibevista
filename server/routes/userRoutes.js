d6675e15f530988d6dec315874197b9216431dd2
Ambuj Shukla <146608389+imambujshukla7@users.noreply.github.com>
2023-10-03 01:32:39 +0530

    Create userRoutes.js

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.createUser);
+
+module.exports = router;
