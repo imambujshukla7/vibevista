a61898d0994b2a4a8ed1b7056ba28aec025671a0
Ambuj Shukla <146608389+imambujshukla7@users.noreply.github.com>
2023-10-03 01:28:11 +0530

    Create userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require('../models/userModel');
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createUser = async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  
+};
